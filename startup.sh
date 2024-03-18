35a2d833ffdd7496e28489339cea9cd5dfb8a4ad
wthlolbbq <wthlolbbq@wth.lol.bbq>
2024-03-18 23:21:32 +0100

    NOTASK/add-startup-script: add startup script & update README

---
+++ startup.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+if ! [ -d altynai ]; then
+  git clone https://GITHUB_ACCESS_TOKEN@github.com/wthlolbbq/altynai.git
+fi
+
+cd altynai
+git fetch
+git pull
+pip install -r requirements.txt
+export PYTHONPATH="${PYTHONPATH}:/home/container/altynai/"
+python bot/main.py
